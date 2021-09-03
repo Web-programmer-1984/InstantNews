6aa57eb89de8b493fb7dacdbf2b50ce59fecaedc
Ayush shukla <89722427+Web-programmer-1984@users.noreply.github.com>
2021-09-03 22:24:09 +0530

    intiallising instant news

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
     return (
       <>
         <NavBar></NavBar>
-        <News pageSize={6}></News>
+        <News pageSize={6} country="in" category='science'></News>
       </>
     )
   }
